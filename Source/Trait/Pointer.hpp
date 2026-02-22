d261addf0e17f2850dbd09ed40ce66e8a4f803b0
verdeving <verodeving@gmail.com>
2026-02-22 01:02:00 +0100

    Fixed builtin support lacking

--- Source/Trait/Pointer.hpp
+++ Source/Trait/Pointer.hpp
@@ -5,7 +5,135 @@
 
 namespace Alice::Trait
 {
-    template<class Self> concept Pointer = __is_pointer(Self);
+    namespace Detail
+    {
+        template<class> struct [[nodiscard]] Pointer final
+        {
+            constexpr ~Pointer() noexcept = delete;
+
+            explicit consteval Pointer() noexcept = delete;
+
+            explicit consteval Pointer(const Pointer&) noexcept = delete;
+
+            explicit consteval Pointer(Pointer&&) noexcept = delete;
+
+            consteval auto operator=(const Pointer&) noexcept -> Pointer& = delete;
+
+            consteval auto operator=(Pointer&&) noexcept -> Pointer& = delete;
+
+            [[nodiscard]] consteval auto operator==(const Pointer&) const noexcept -> bool = delete
+            ;
+
+            [[nodiscard]] consteval auto operator<=>(const Pointer&) const noexcept = delete;
+
+            [[nodiscard]] static consteval auto Value() noexcept -> bool
+            {
+                return false;
+            }
+        };
+
+        template<class Self> struct [[nodiscard]] Pointer<Self*> final
+        {
+            constexpr ~Pointer() noexcept = delete;
+
+            explicit consteval Pointer() noexcept = delete;
+
+            explicit consteval Pointer(const Pointer&) noexcept = delete;
+
+            explicit consteval Pointer(Pointer&&) noexcept = delete;
+
+            consteval auto operator=(const Pointer&) noexcept -> Pointer& = delete;
+
+            consteval auto operator=(Pointer&&) noexcept -> Pointer& = delete;
+
+            [[nodiscard]] consteval auto operator==(const Pointer&) const noexcept -> bool = delete
+            ;
+
+            [[nodiscard]] consteval auto operator<=>(const Pointer&) const noexcept = delete;
+
+            [[nodiscard]] static consteval auto Value() noexcept -> bool
+            {
+                return true;
+            }
+        };
+
+        template<class Self> struct [[nodiscard]] Pointer<Self* const> final
+        {
+            constexpr ~Pointer() noexcept = delete;
+
+            explicit consteval Pointer() noexcept = delete;
+
+            explicit consteval Pointer(const Pointer&) noexcept = delete;
+
+            explicit consteval Pointer(Pointer&&) noexcept = delete;
+
+            consteval auto operator=(const Pointer&) noexcept -> Pointer& = delete;
+
+            consteval auto operator=(Pointer&&) noexcept -> Pointer& = delete;
+
+            [[nodiscard]] consteval auto operator==(const Pointer&) const noexcept -> bool = delete
+            ;
+
+            [[nodiscard]] consteval auto operator<=>(const Pointer&) const noexcept = delete;
+
+            [[nodiscard]] static consteval auto Value() noexcept -> bool
+            {
+                return true;
+            }
+        };
+
+        template<class Self> struct [[nodiscard]] Pointer<Self* volatile> final
+        {
+            constexpr ~Pointer() noexcept = delete;
+
+            explicit consteval Pointer() noexcept = delete;
+
+            explicit consteval Pointer(const Pointer&) noexcept = delete;
+
+            explicit consteval Pointer(Pointer&&) noexcept = delete;
+
+            consteval auto operator=(const Pointer&) noexcept -> Pointer& = delete;
+
+            consteval auto operator=(Pointer&&) noexcept -> Pointer& = delete;
+
+            [[nodiscard]] consteval auto operator==(const Pointer&) const noexcept -> bool = delete
+            ;
+
+            [[nodiscard]] consteval auto operator<=>(const Pointer&) const noexcept = delete;
+
+            [[nodiscard]] static consteval auto Value() noexcept -> bool
+            {
+                return true;
+            }
+        };
+
+        template<class Self> struct [[nodiscard]] Pointer<Self* volatile const> final
+        {
+            constexpr ~Pointer() noexcept = delete;
+
+            explicit consteval Pointer() noexcept = delete;
+
+            explicit consteval Pointer(const Pointer&) noexcept = delete;
+
+            explicit consteval Pointer(Pointer&&) noexcept = delete;
+
+            consteval auto operator=(const Pointer&) noexcept -> Pointer& = delete;
+
+            consteval auto operator=(Pointer&&) noexcept -> Pointer& = delete;
+
+            [[nodiscard]] consteval auto operator==(const Pointer&) const noexcept -> bool = delete
+            ;
+
+            [[nodiscard]] consteval auto operator<=>(const Pointer&) const noexcept = delete;
+
+            [[nodiscard]] static consteval auto Value() noexcept -> bool
+            {
+                return true;
+            }
+        };
+    }
+
+    template<class Self> concept Pointer = Detail::Pointer<Self>::Value();
 }
 
 #endif

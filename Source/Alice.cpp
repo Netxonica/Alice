22dbbe31a5962561bfc12be0cbf24d9dfe218262
verdeving <verodeving@gmail.com>
2026-04-11 21:17:39 +0200

    Added entry point and checker for 1 byte being 8 bits

---
+++ Source/Alice.cpp
@@ -0,0 +1,20 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if alice_major >= 0 and alice_middle >= 0 and alice_minor >= 1
+#include <climits>
+
+static_assert(CHAR_BIT == 8, "each byte must be 8-bits");
+
+#ifdef alice_windows
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+#else
+int main()
+#endif
+{
+    #ifdef alice_windows
+    return 0;
+    #endif
+}
+
+#endif

9e5ebc6ec1c32c82788018d83355713e37a8f52e
verdeving <verodeving@gmail.com>
2026-05-01 11:04:43 +0200

    Added function trait

--- Source/Trait/Function.hpp
+++ Source/Trait/Function.hpp
@@ -2,7 +2,7 @@
 #if alice_major >= 0 and alice_middle >= 0 and alice_minor >= 1
 #ifndef alice_header_guard_trait_function
 #define alice_header_guard_trait_function
-#ifdef __MSC_VER
+#ifdef _MSC_VER
 #include "Trait/Constant.hpp"
 #include "Trait/Reference.hpp"
 #endif

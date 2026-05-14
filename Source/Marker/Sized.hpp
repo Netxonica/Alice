eb8c0eeb00e71766299c4157cfe811bca87d4a3d
verdeving <verodeving@gmail.com>
2026-05-14 02:01:04 +0200

    Added forward to sized

--- Source/Marker/Sized.hpp
+++ Source/Marker/Sized.hpp
@@ -2,6 +2,7 @@
 #if alice_major >= 0 and alice_middle >= 0 and alice_minor >= 1
 #ifndef alice_header_guard_marker_sized
 #define alice_header_guard_marker_sized
+#include "Core/Forward.hpp"
 #include "Trait/Function.hpp"
 #include "Meta/RemoveExtents.hpp"
 #include "Trait/UnboundedArray.hpp"
@@ -15,7 +16,7 @@ namespace Alice::Marker
     template<class Self> concept Sized = not Trait::Function<Self> and not Trait::UnboundedArray<
     Self> and requires(Meta::RemoveExtents<Self> self)
     {
-        sizeof(self);
+        sizeof($forward(self));
     };
 }
 

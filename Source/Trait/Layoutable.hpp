cf66ff0fbd145a722ea9fe099f54cb0e399d5871
verdeving <verodeving@gmail.com>
2026-06-30 12:58:24 +0200

    Added layoutable trait

--- Source/Trait/Layoutable.hpp
+++ Source/Trait/Layoutable.hpp
@@ -2,13 +2,16 @@
 #if alice_major >= 0 and alice_middle >= 0 and alice_minor >= 1
 #ifndef alice_header_guard_trait_layoutable
 #define alice_header_guard_trait_layoutable
+#include "Marker/Sized.hpp"
 
 namespace Alice::Trait
 {
     /**
-     * @brief Satisfied when @p Self is layout-compatible with @p Type
+     * @brief Satisfied when both @p Self and @p Type satisfy Alice::Marker::Sized, and @p Self is
+     * layout-compatible with @p Type
      */
-    template<class Self, class Type> concept Layoutable = __is_layout_compatible(Self, Type);
+    template<class Self, class Type> concept Layoutable = Marker::Sized<Self> and Marker::Sized<
+    Type> and __is_layout_compatible(Self, Type);
 }
 
 #endif

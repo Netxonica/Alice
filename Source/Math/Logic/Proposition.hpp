7c6e946a7c33853288f86ebb91140a9d44be2a24
verdeving <verodeving@gmail.com>
2026-09-05 09:13:36 +0200

    [Math][Logic] Adapted Proposition for MSVC's lack of delete('message')

--- Source/Math/Logic/Proposition.hpp
+++ Source/Math/Logic/Proposition.hpp
@@ -38,17 +38,17 @@ namespace Alice::Math::Logic
          */
         constexpr Proposition(Proposition&& other) noexcept = default;
 
-        consteval auto operator=(const Proposition&) noexcept -> Proposition& = delete(
+        consteval auto operator=(const Proposition&) noexcept -> Proposition& = delete
         #ifndef _MSC_VER
-        "CPL doesn't define assignment operations"
+        ("CPL doesn't define assignment operations")
         #endif
-        );
+        ;
 
-        consteval auto operator=(Proposition&&) noexcept -> Proposition& = delete(
+        consteval auto operator=(Proposition&&) noexcept -> Proposition& = delete
         #ifndef _MSC_VER
-        "CPL doesn't define assignment operations"
+        ("CPL doesn't define assignment operations")
         #endif
-        );
+        ;
 
         [[nodiscard]] consteval auto operator not_eq(const Proposition&) const noexcept -> bool =
         delete;

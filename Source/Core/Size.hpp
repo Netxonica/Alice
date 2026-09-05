e4a741b4daf2b6ebbc38af86215e9f9f2496073d
verdeving <verodeving@gmail.com>
2026-09-05 11:13:55 +0200

    [Core] Added more operators to Size

--- Source/Core/Size.hpp
+++ Source/Core/Size.hpp
@@ -2,8 +2,7 @@
 #if alice_major >= 0 and alice_middle >= 0 and alice_minor >= 1
 #ifndef alice_header_guard_core_size
 #define alice_header_guard_core_size
-#include "Core/Inline.hpp"
-#include "Core/Safety.hpp"
+#include "Math/Logic/Proposition.hpp"
 
 namespace Alice
 {
@@ -78,6 +77,286 @@ namespace Alice
         }
 
         /**
+         * @brief Computes the unary plus.
+         */
+        [[nodiscard]] constexpr auto operator+() const noexcept -> Size
+        {
+            return Size{m_value};
+        }
+
+        /**
+         * @brief Computes the pre-increment by 1.
+         */
+        constexpr auto operator++() noexcept -> Size&
+        {
+            ++m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the post-increment by 1.
+         */
+        constexpr auto operator++(int) noexcept -> Size
+        {
+            return Size{m_value++};
+        }
+
+        /**
+         * @brief Computes the pre-decrement by 1.
+         */
+        constexpr auto operator--() noexcept -> Size&
+        {
+            --m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the post-decrement by 1.
+         */
+        constexpr auto operator--(int) noexcept -> Size
+        {
+            return Size{m_value--};
+        }
+
+        /**
+         * @brief Computes the arithmetic addition and assigment.
+         */
+        constexpr auto operator+=(const Size rhs) noexcept -> Size&
+        {
+            m_value += rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the arithmetic addition.
+         */
+        [[nodiscard]] constexpr auto operator+(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value + rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the arithmetic subtraction and assigment.
+         */
+        constexpr auto operator-=(const Size rhs) noexcept -> Size&
+        {
+            m_value -= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the arithmetic subtraction.
+         */
+        [[nodiscard]] constexpr auto operator-(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value - rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the arithmetic multiplication and assigment.
+         */
+        constexpr auto operator*=(const Size rhs) noexcept -> Size&
+        {
+            m_value *= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the arithmetic multiplication.
+         */
+        [[nodiscard]] constexpr auto operator*(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value * rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the arithmetic division and assigment.
+         */
+        constexpr auto operator/=(const Size rhs) noexcept -> Size&
+        {
+            m_value /= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the arithmetic division.
+         */
+        [[nodiscard]] constexpr auto operator/(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value / rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the arithmetic modulo and assigment.
+         */
+        constexpr auto operator%=(const Size rhs) noexcept -> Size&
+        {
+            m_value %= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the arithmetic modulo.
+         */
+        [[nodiscard]] constexpr auto operator%(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value % rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise negation.
+         */
+        [[nodiscard]] constexpr auto operator compl() const noexcept -> Size
+        {
+            return Size{compl m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise conjunction and assignment.
+         */
+        constexpr auto operator and_eq(const Size rhs) noexcept -> Size&
+        {
+            m_value and_eq rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the bitwise conjunction.
+         */
+        [[nodiscard]] constexpr auto operator bitand(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value bitand rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise disjunction and assignment.
+         */
+        constexpr auto operator or_eq(const Size rhs) noexcept -> Size&
+        {
+            m_value or_eq rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the bitwise disjunction.
+         */
+        [[nodiscard]] constexpr auto operator bitor(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value bitor rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise exclusive disjunction and assignment.
+         */
+        constexpr auto operator xor_eq(const Size rhs) noexcept -> Size&
+        {
+            m_value xor_eq rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the bitwise exclusive disjunction.
+         */
+        [[nodiscard]] constexpr auto operator xor(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value xor rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise left shift and assignment.
+         */
+        constexpr auto operator<<=(const Size rhs) noexcept -> Size&
+        {
+            m_value <<= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the bitwise left shift.
+         */
+        [[nodiscard]] constexpr auto operator<<(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value << rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the bitwise right shift and assignment.
+         */
+        constexpr auto operator>>=(const Size rhs) noexcept -> Size&
+        {
+            m_value >>= rhs.m_value;
+            return *this;
+        }
+
+        /**
+         * @brief Computes the bitwise right shift.
+         */
+        [[nodiscard]] constexpr auto operator>>(const Size rhs) const noexcept -> Size
+        {
+            return Size{m_value >> rhs.m_value};
+        }
+
+        /**
+         * @brief Computes the less-than comparison.
+         */
+        [[nodiscard]] constexpr auto operator<(const Size rhs) const noexcept -> Math::Logic::
+        Proposition
+        {
+            return m_value < rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic::
+            Proposition::Contradiction();
+        }
+
+        /**
+         * @brief Computes the less-than-or-equal-to comparison.
+         */
+        [[nodiscard]] constexpr auto operator<=(const Size rhs) const noexcept -> Math::Logic::
+        Proposition
+        {
+            return m_value <= rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic::
+            Proposition::Contradiction();
+        }
+
+        /**
+         * @brief Computes the greater-than comparison.
+         */
+        [[nodiscard]] constexpr auto operator>(const Size rhs) const noexcept -> Math::Logic::
+        Proposition
+        {
+            return m_value > rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic::
+            Proposition::Contradiction();
+        }
+
+        /**
+         * @brief Computes the greater-than-or-equal-to comparison.
+         */
+        [[nodiscard]] constexpr auto operator>=(const Size rhs) const noexcept -> Math::Logic::
+        Proposition
+        {
+            return m_value >= rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic::
+            Proposition::Contradiction();
+        }
+
+        /**
+         * @brief Computes the equality comparison.
+         */
+        [[nodiscard]] constexpr auto operator==(const Size rhs) const noexcept -> Math::Logic::
+        Proposition
+        {
+            return m_value == rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic::
+            Proposition::Contradiction();
+        }
+
+        /**
+         * @brief Computes the inequality comparison.
+         */
+        [[nodiscard]] constexpr auto operator not_eq(const Size rhs) const noexcept -> Math::Logic
+        ::Proposition
+        {
+            return m_value not_eq rhs.m_value ? Math::Logic::Proposition::Tautology() : Math::Logic
+            ::Proposition::Contradiction();
+        }
+
+        /**
          * @brief The smallest value that can be represented.
          */
         [[nodiscard]] static consteval auto Minimum() noexcept -> Size
@@ -90,7 +369,7 @@ namespace Alice
          */
         [[nodiscard]] static consteval auto Maximum() noexcept -> Size
         {
-            return Size{18446744073709551615uz};
+            return Size{18'446'744'073'709'551'615uz};
         }
 
         /**
@@ -100,7 +379,27 @@ namespace Alice
         {
             return Size{64uz};
         }
+
+        /**
+         * @brief The maximum number of digits of this integer type in decimal base.
+         */
+        [[nodiscard]] static consteval auto Digits() noexcept -> Size
+        {
+            return Size{20uz};
+        }
     };
+
+    namespace Literals
+    {
+        /**
+         * @brief The UDL constructing a @ref Size from a literal.
+         */
+        [[nodiscard]] consteval auto operator""_size(const unsigned long long value) noexcept ->
+        Size
+        {
+            return Size{static_cast<Size::Native>(value)};
+        }
+    }
 }
 
 #endif
--- Test/Core/Size.cpp
+++ Test/Core/Size.cpp
@@ -55,6 +55,223 @@ using Alice::Size;
         if(static_cast<Size::Native>(skibidi) not_eq 67uz)
             return false;
     }
+    // UDL
+    {
+        using namespace Alice::Literals;
+        Size skibidi = 67_size;
+        if(static_cast<Size::Native>(skibidi) not_eq 67uz)
+            return false;
+    }
+    // Unary plus
+    {
+        Size skibidi{67uz};
+        if(static_cast<Size::Native>(+skibidi) not_eq 67uz)
+            return false;
+    }
+    // Pre-increment
+    {
+        Size skibidi{67uz};
+        if(static_cast<Size::Native>(skibidi) not_eq 67uz)
+            return false;
+        if(static_cast<Size::Native>(++skibidi) not_eq 68uz)
+            return false;
+    }
+    // Post-increment
+    {
+        Size skibidi{67uz};
+        if(static_cast<Size::Native>(skibidi++) not_eq 67uz)
+            return false;
+        if(static_cast<Size::Native>(skibidi) not_eq 68uz)
+            return false;
+    }
+    // Pre-decrement
+    {
+        Size skibidi{67uz};
+        if(static_cast<Size::Native>(skibidi) not_eq 67uz)
+            return false;
+        if(static_cast<Size::Native>(--skibidi) not_eq 66uz)
+            return false;
+    }
+    // Post-decrement
+    {
+        Size skibidi{67uz};
+        if(static_cast<Size::Native>(skibidi--) not_eq 67uz)
+            return false;
+        if(static_cast<Size::Native>(skibidi) not_eq 66uz)
+            return false;
+    }
+    // Addition and assignment
+    {
+        Size skibidi{67uz}, fanum{42uz};
+        skibidi += fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 109uz)
+            return false;
+    }
+    // Addition
+    {
+        Size skibidi{67uz}, fanum{42uz}, meow = skibidi + fanum;
+        if(static_cast<Size::Native>(meow) not_eq 109uz)
+            return false;
+    }
+    // Subtraction and assignment
+    {
+        Size skibidi{67uz}, fanum{42uz};
+        skibidi -= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 25uz)
+            return false;
+    }
+    // Subtraction
+    {
+        Size skibidi{67uz}, fanum{42uz}, meow = skibidi - fanum;
+        if(static_cast<Size::Native>(meow) not_eq 25uz)
+            return false;
+    }
+    // Multiplication and assignment
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        skibidi *= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 420uz)
+            return false;
+    }
+    // Multiplication
+    {
+        Size skibidi{42uz}, fanum{10uz}, meow = skibidi * fanum;
+        if(static_cast<Size::Native>(meow) not_eq 420uz)
+            return false;
+    }
+    //Division and assignment
+    {
+        Size skibidi{40uz}, fanum{10uz};
+        skibidi /= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 4uz)
+            return false;
+    }
+    // Division
+    {
+        Size skibidi{40uz}, fanum{10uz}, meow = skibidi / fanum;
+        if(static_cast<Size::Native>(meow) not_eq 4uz)
+            return false;
+    }
+    // Modulo and assignment
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        skibidi %= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 2uz)
+            return false;
+    }
+    // Modulo
+    {
+        Size skibidi{42uz}, fanum{10uz}, meow = skibidi % fanum;
+        if(static_cast<Size::Native>(meow) not_eq 2uz)
+            return false;
+    }
+    // Bitwise NOT
+    {
+        Size skibidi(42uz), fanum = compl skibidi;
+        if(static_cast<Size::Native>(fanum) not_eq 18446744073709551573uz)
+            return false;
+    }
+    // Bitwise AND and assignment
+    {
+        Size skibidi(42uz), fanum{10uz};
+        skibidi and_eq fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 10uz)
+            return false;
+    }
+    // Bitwise AND
+    {
+        Size skibidi(42uz), fanum{10uz}, meow = skibidi bitand fanum;
+        if(static_cast<Size::Native>(meow) not_eq 10uz)
+            return false;
+    }
+    // Bitwise OR and assignment
+    {
+        Size skibidi(42uz), fanum{10uz};
+        skibidi or_eq fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 42uz)
+            return false;
+    }
+    // Bitwise OR
+    {
+        Size skibidi(42uz), fanum{10uz}, meow = skibidi bitor fanum;
+        if(static_cast<Size::Native>(meow) not_eq 42uz)
+            return false;
+    }
+    // Bitwise XOR and assignment
+    {
+        Size skibidi(42uz), fanum{10uz};
+        skibidi xor_eq fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 32uz)
+            return false;
+    }
+    // Bitwise XOR
+    {
+        Size skibidi(42uz), fanum{10uz}, meow = skibidi xor fanum;
+        if(static_cast<Size::Native>(meow) not_eq 32uz)
+            return false;
+    }
+    // Bitwise left shift and assignment
+    {
+        Size skibidi(42uz), fanum{10uz};
+        skibidi <<= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 43008uz)
+            return false;
+    }
+    // Bitwise left shift
+    {
+        Size skibidi(42uz), fanum{10uz}, meow = skibidi << fanum;
+        if(static_cast<Size::Native>(meow) not_eq 43008uz)
+            return false;
+    }
+    // Bitwise right shift and assignment
+    {
+        Size skibidi(42uz), fanum{1uz};
+        skibidi >>= fanum;
+        if(static_cast<Size::Native>(skibidi) not_eq 21uz)
+            return false;
+    }
+    // Bitwise right shift
+    {
+        Size skibidi(42uz), fanum{1uz}, meow = skibidi >> fanum;
+        if(static_cast<Size::Native>(meow) not_eq 21uz)
+            return false;
+    }
+    // Less-than comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(static_cast<bool>(skibidi < fanum))
+            return false;
+    }
+    // Less-than-or-equal-to comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(static_cast<bool>(skibidi <= fanum))
+            return false;
+    }
+    // Greater-than comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(not static_cast<bool>(skibidi > fanum))
+            return false;
+    }
+    // Greater-than-or-equal-to comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(not static_cast<bool>(skibidi >= fanum))
+            return false;
+    }
+    // Equality comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(static_cast<bool>(skibidi == fanum))
+            return false;
+    }
+    // Inequality comparison
+    {
+        Size skibidi{42uz}, fanum{10uz};
+        if(not static_cast<bool>(skibidi not_eq fanum))
+            return false;
+    }
     // Minimum value
     {
         Size skibidi = Size::Minimum();
@@ -64,13 +281,19 @@ using Alice::Size;
     // Maximum value
     {
         Size skibidi = Size::Maximum();
-        if(static_cast<Size::Native>(skibidi) not_eq 18446744073709551615uz)
+        if(static_cast<Size::Native>(skibidi) not_eq 18'446'744'073'709'551'615uz)
             return false;
     }
     // Bits value
     {
         Size skibidi = Size::Bits();
         if(static_cast<Size::Native>(skibidi) not_eq 64uz)
+            return false;
+    }
+    // Digits value
+    {
+        Size skibidi = Size::Digits();
+        if(static_cast<Size::Native>(skibidi) not_eq 20uz)
             return false;
     }
     return true;
